46e3cdf194ac4dcfe8409a83778f36fba4cfed1d
Michał Muskała <michal@muskala.eu>
2017-08-08 09:25:29 +0200

    Clean up the example handler a bit

--- examples/dummy_handler.ex
+++ examples/dummy_handler.ex
@@ -1,102 +1,93 @@
 defmodule RestAuth.DummyHandler do
-  @behaviour RestAuth.HandlerBehaviour
-
-  @fake_user %{
-                name: "Fake User",
-                password: "acceptme", #cleartext, do NOT do this, use Comeonin or similar!
-                company_id: 33
-              }
-
-  require Logger
-  alias RestAuth.CacheService
-
   @moduledoc """
   Sample Handler module showing intended flow and a sample set of
-  internal helpers. Will throw warnings if used in an actual app.
+  internal helpers.
+
   Intention is to be inspiration for your own handler module.
   """
 
+  @behaviour RestAuth.HandlerBehaviour
+
+  @fake_user %{
+    name: "Fake User",
+    password: "acceptme", # cleartext, do NOT do this, use Comeonin or similar!
+    company_id: 33
+  }
+
+  require Logger
+  alias RestAuth.CacheService
 
   ################################
   # BEHAVIOUR REQUIRED FUNCTIONS #
   ################################
   def load_user_data(username, raw_password) do
+    # This is normally a DB lookup based on the username
+    user =
+      if username == "fakeuser@example.com" do
+        @fake_user
+      else
+        nil
+      end
 
-    #This is normally a DB lookup based on the username
-    if username == "fakeuser@example.com" do
-      @fake_user
-    else
-      nil
-    end
-    |> case do
+    case user do
       nil ->
+        dummy_check_password()
         {:error, "User not found or wrong password"}
       user ->
-        case check_password(user.password, raw_password) do
-          true ->
-            Logger.debug "Password accepted, loading user data"
-            load_user_data(user)
-          false ->
-            Logger.debug "#{user.username}[#{user.name}] did not match password"
-            {:error, "User not found or wrong password"}
+        if check_password(user.password, raw_password) do
+          Logger.debug "Password accepted, loading user data"
+          load_user_data(user)
+        else
+          Logger.debug "#{user.username}[#{user.name}] did not match password"
+          {:error, "User not found or wrong password"}
         end
     end
   end
 
   def load_user_data(user) do
     metadata = %{
-      "name" => user.name,
-      "company_id" => user.company_id,
+      name: user.name,
+      company_id: user.company_id,
     }
 
     # We put a versioning in the token signature
-    # to be able to do changes to our token strategy later.
+    # to be able to easily do changes to our token strategy later.
     token_data = %{user_id: user.id, version: 1}
-                 |> Poison.encode!
     token = Phoenix.Token.sign(RestAuth.FakeEndpoint, get_salt(), token_data)
 
-    authority = %RestAuth.Authority{
-                  token: token,
-                  roles: get_roles(user),
-                  user_id: user.id,
-                  metadata: metadata,
-                  anonymous: false
-                }
+    authority =
+      %RestAuth.Authority{
+        token: token,
+        roles: get_roles(user),
+        user_id: user.id,
+        metadata: metadata,
+        anonymous: false
+      }
     write_token_to_db(authority)
     CacheService.put_user(authority)
     {:ok, authority}
   end
 
-  @doc """
-  Decodes a token
-  """
   def load_user_data_from_token(token) do
     case Phoenix.Token.verify(RestAuth.FakeEndpoint, get_salt(), token) do
-      {:ok, token_data_raw} ->
-        Logger.debug "token_data_raw is #{inspect token_data_raw}"
-        case Poison.decode(token_data_raw) do
-          {:ok, %{"user_id" => user_id, "version" => _}} ->
-            authority = %RestAuth.Authority{
-                          user_id: user_id,
-                          token: token
-                        }
-            case CacheService.get_user(authority) do
-              :not_found ->
-                load_token_from_db(authority)
-                |> case do
-                  nil ->
-                    {:error, :invalid}
-                  authority ->
-                    CacheService.put_user(authority)
-                    {:ok, authority}
-                end
-              {:ok, authority} ->
+      {:ok, %{user_id: user_id, version: 1}} ->
+        authority = %RestAuth.Authority{user_id: user_id, token: token}
+
+        case CacheService.get_user(authority) do
+          :not_found ->
+            case load_token_from_db(authority) do
+              nil ->
+                {:error, "token invalid"}
+              authority ->
+                CacheService.put_user(authority)
                 {:ok, authority}
             end
-          {:ok, _} ->
-            {:error, "token expired"}
+          {:ok, authority} ->
+            {:ok, authority}
         end
-      #Both error conditions will halt the plug pipeline
+      {:ok, _} ->
+        {:error, "token expired"}
+      # Error conditions will halt the plug pipeline
       {:error, :expired} ->
         {:error, "token expired"}
       {:error, :invalid} ->
@@ -104,35 +95,20 @@ defmodule RestAuth.DummyHandler do
     end
   end
 
-
-
-  #Defined with some default values to appease the behaviour gods
-  def can_access_item?(%RestAuth.Authority{user_id: _user_id}, _category, _target_id), do: true
-  def invalidate_user_acl(%RestAuth.Authority{user_id: _user_id}), do: :ok
-
   def invalidate_token(authority = %RestAuth.Authority{}) do
     delete_token_from_db(authority)
-    CacheService.invalidate_token(authority)
-    |> case do
+    case CacheService.invalidate_token(authority) do
       {:error, nodes} ->
         Logger.error "Not able to delete tokens on nodes: #{inspect nodes}"
         {:error, "Failed to delete on #{length(nodes)} node(s)"}
-      _ ->
+      {:ok, _} ->
         :ok
     end
   end
 
-  def invalidate_user(authority = %RestAuth.Authority{}) do
-    delete_all_tokens_from_db(authority)
-    CacheService.invalidate_user(authority)
-    |> case do
-      {:error, nodes} ->
-        Logger.error "Not able to delete tokens on nodes: #{inspect nodes}"
-        {:error, "Failed to delete on #{length(nodes)} node(s)"}
-      _ ->
-        :ok
-    end
-  end
+  ####################################
+  # OPTIONAL CONFIGURATION CALLBACKS #
+  ####################################
 
   def write_cookie?(), do: false
 
@@ -140,18 +116,45 @@ defmodule RestAuth.DummyHandler do
 
   def anonymous_roles(), do: []
 
+  ######################
+  # OPTIONAL CALLBACKS #
+  ######################
+
+  # These are not used by the library directly, but are a suggestion on
+  # possible further usage in your application
+
+  def can_access_item?(%RestAuth.Authority{user_id: _user_id}, _category, _target_id), do: true
+
+  def invalidate_user_acl(%RestAuth.Authority{user_id: _user_id}), do: :ok
+
+  def invalidate_user(authority = %RestAuth.Authority{}) do
+    delete_all_tokens_from_db(authority)
+    case CacheService.invalidate_user(authority) do
+      {:error, nodes} ->
+        Logger.error "Not able to delete tokens on nodes: #{inspect nodes}"
+        {:error, "Failed to delete on #{length(nodes)} node(s)"}
+      {:ok, _} ->
+        :ok
+    end
+  end
+
   ###########
   # HELPERS #
   ###########
 
   defp get_salt() do
-    "set_your_own_loooong_salt_here"
+    "set_your_own_loooong_salt_here_or_lookup_in_endpoint_config"
   end
 
-
   defp check_password(encoded_password, raw_password) do
-    #This should normally lean on Comeonin or similar libraries
+    # This should normally lean on Comeonin or similar libraries
     encoded_password == raw_password
+  end
+
+  defp dummy_check_password() do
+    # This should simulate a similar computational cost to check_password
+    # to prevent timing attacks
+    true
   end
 
   defp get_roles(_user) do
@@ -159,27 +162,27 @@ defmodule RestAuth.DummyHandler do
     ["user", "admin"]
   end
 
-  #Writes to cache and to db
-  defp write_token_to_db(authority) do
+  defp write_token_to_db(_authority) do
     # Write the authority to database here.
     # Either attempt to load based on .user_id and .token before
     # writing or put in a unique constraint to make sure you
     # dont end up with multiple identical tokens.
-    authority
+    :ok
   end
 
   defp load_token_from_db(authority) do
     # Just setting some metadata as a sample here.
     # This function should normally reconstruct the entire
     # Authority from the database.
-    %{authority | metadata: %{"name" => @fake_user.name}}
+    %{authority | metadata: %{name: @fake_user.name}}
   end
 
-  defp delete_token_from_db(%RestAuth.Authority{user_id: _user_id, token: _token}) do
-    #Function included as sample
+  defp delete_token_from_db(_authority) do
+    # Counterpart to write_token_to_db/1
+    :ok
   end
 
-  defp delete_all_tokens_from_db(%RestAuth.Authority{user_id: _user_id}) do
-    #Function included as sample
+  defp delete_all_tokens_from_db(_authority) do
+    :ok
   end
 end
